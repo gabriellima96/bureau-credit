4b6b72b637d6f4e7f2526fef6cfee0084efae301
gabriellima96 <gabriellima.silva96@gmail.com>
2020-04-18 20:07:58 -0300

    [FEAT][#NOISSUE] - Criando e configurando o microservice basea

---
+++ bureau-credit-base-a/src/main/resources/db/migration/V01__create_schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE PESSOA  (
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(120) NOT NULL,
+    cpf VARCHAR(11) NOT NULL,
+    data_nascimento DATE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+ 
+CREATE TABLE ENDERECO  (
+	pessoa_id BIGINT(20),
+	logradouro VARCHAR(255),
+	bairro VARCHAR(255),
+	localidade VARCHAR(255),
+	uf CHAR(2),
+	complemento VARCHAR(255),
+	cep VARCHAR(9),
+	numero VARCHAR(100),
+	UNIQUE INDEX (pessoa_id),
+	FOREIGN KEY (pessoa_id) REFERENCES PESSOA(id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE DIVIDA  (
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	pessoa_id BIGINT(20),
+    descricao VARCHAR(255) NOT NULL,
+    valor DECIMAL(11, 2) NOT NULL,
+    data_vencimento DATE,
+    status ENUM('PENDENTE', 'NEGOCIACAO') NOT NULL DEFAULT 'PENDENTE',
+    FOREIGN KEY (pessoa_id) REFERENCES PESSOA(id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
---
+++ bureau-credit-base-a/src/test/java/site/gabriellima/bureau/BureauCreditBaseAApplicationTests.java
@@ -0,0 +1,13 @@
+package site.gabriellima.bureau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BureauCreditBaseAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
